a3372709bee82de63fd3dea923f1c544535d4292
Kali-Brook-SE <abelseyoum.education@gmail.com>
2022-03-17 12:05:50 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int i = 1;
 
-	for (; i <= 100; i++)
+	for (; i < 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
@@ -21,5 +21,7 @@ int main(void)
 		else
 			printf("%d ", i);
 	}
+	printf("Buzz\n");
+
 	return (0);
 }
